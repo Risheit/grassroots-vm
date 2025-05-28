7d538d5b80e42d62c128b96d7a31176d3b5602dc
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2025-05-28 17:24:18 -0500

    Add grassroots assembler boilerplate

---
+++ gsm/main.c
@@ -0,0 +1,14 @@
+
+/**
+ * Step 1: Open GBC File for parsing
+ * Step 2: Read until "code section begins" for metadata.
+ * Step 3: Read data section into memory.
+ * Step 4: Read instructions line by line.
+ * Step 5: Perform based on instructions
+ *
+ */
+
+int main() {
+
+  return 0;
+}

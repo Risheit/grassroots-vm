e98bc0445b548092cec1787abdf2db24fa5cbbef
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2025-10-03 03:23:52 -0400

    Begin adding GSM second pass

---
+++ gsm/gbc_constants.h
@@ -0,0 +1,137 @@
+#include <stdint.h>
+
+// Type definitions for a GBC command. See
+// https://github.com/Risheit/grassroots-vm/wiki/GBC-to-GA-Language-Specifications
+typedef uint32_t gbc_command;
+
+// Masks for various parts of a GBC command.
+
+#define OPCODE_MASK 0x0000003F
+#define OPCODE_SHFT 0
+
+// Code 1 Masks
+
+#define CODE1_FNC 0x000007C0
+#define CODE1_FNC_SHFT 6
+
+#define CODE1_RX1 0x0000F800
+#define CODE1_RX1_SHFT 11
+
+#define CODE1_IMM 0xFFFF0000
+#define CODE1_IMM_SHFT 16
+
+// Code 2 Masks
+
+#define CODE2_RX1 0x000007C0
+#define CODE2_RX1_SHFT 6
+
+#define CODE2_RX2 0x0000F800
+#define CODE2_RX2_SHFT 11
+
+#define CODE2_IMM 0xFFFF0000
+#define CODE2_IMM_SHFT 16
+
+// Code 3 Masks
+
+#define CODE3_FNC 0x00003FC0
+#define CODE3_FNC_SHFT 6
+
+#define CODE3_XXX 0x0001C000 // Unused
+#define CODE3_XXX_SHFT 14
+
+#define CODE3_RX1 0x003E0000
+#define CODE3_RX1_SHFT 17
+
+#define CODE3_RX2 0x07C00000
+#define CODE3_RX2_SHFT 22
+
+#define CODE3_RX3 0xF8000000
+#define CODE3_RX3_SHFT 27
+
+// Opcode representations
+
+#define OPCODE_CODE1 0x3F
+#define OPCODE_CODE3 0x00
+#define OPCODE_ADDI 0x02
+#define OPCODE_SUBI 0x03
+#define OPCODE_BLI 0x06
+#define OPCODE_BRI 0x07
+#define OPCODE_BRIU 0x08
+#define OPCODE_ANDI 0x09
+#define OPCODE_ORI 0x0A
+#define OPCODE_XORI 0x0B
+#define OPCODE_LW 0x0C
+#define OPCODE_LH 0x0D
+#define OPCODE_LB 0x0E
+#define OPCODE_SW 0x0F
+#define OPCODE_SH 0x10
+#define OPCODE_SB 0x11
+
+// Code 1 function representations
+
+#define FUNC_LDU 0x01
+#define FUNC_JR 0x02
+#define FUNC_JRC 0x03
+#define FUNC_JRCX 0x04
+#define FUNC_PUSH 0x05
+#define FUNC_POP 0x06
+#define FUNC_ZERO 0x07
+#define FUNC_SYS 0x08
+
+// Code 3 function representations
+
+#define FUNC_ADD 0x01
+#define FUNC_SUB 0x02
+#define FUNC_MUL 0x03
+#define FUNC_MULU 0x04
+#define FUNC_DIV 0x05
+#define FUNC_BLX 0x09
+#define FUNC_BRX 0x0A
+#define FUNC_BRXU 0x0B
+#define FUNC_AND 0x0C
+#define FUNC_OR 0x0D
+#define FUNC_XOR 0x0E
+#define FUNC_EQ 0x0F
+#define FUNC_GT 0x10
+#define FUNC_LT 0x11
+#define FUNC_EQX 0x12
+#define FUNC_GTX 0x13
+#define FUNC_LTX 0x14
+#define FUNC_ADDF 0x15
+#define FUNC_SUBF 0x16
+#define FUNC_MULF 0x17
+#define FUNC_MULFU 0x18
+#define FUNC_DIVF 0x19
+
+// Register representations
+
+#define R_AX 0x01
+#define R_BX 0x02
+#define R_CX 0x03
+#define R_DX 0x04
+#define R_EX 0x05
+#define R_FX 0x06
+#define R_GX 0x07
+#define R_HX 0x08
+
+#define R_SAX 0x09
+#define R_SBX 0x0A
+#define R_SCX 0x0B
+#define R_SDX 0x0C
+
+#define R_RET 0x0D
+#define R_UP 0x0E
+#define R_ZERO 0x0F
+#define R_IF 0x10
+#define R_TG 0x11
+#define R_SP 0x12
+#define R_SRG 0x13
+#define R_ASM 0x14
+#define R_PC 0x15
+
+const uint32_t g_gsm_guard = 0x0a0d0d0a;
+const uint32_t g_gsm_magic = 0x4F434247;
+const uint8_t g_gsm_major = 1;
+const uint8_t g_gsm_minor = 0;
+const uint8_t g_gsm_patch = 0;
+const uint8_t g_gsm_unused = 0;
--- gsm/main.c
+++ gsm/main.c
@@ -25,19 +25,13 @@
 // 1. Remove any created artefacts:
 //    - .gbc file.
 
+#include "gbc_constants.h"
 #include "std/cli.h"
 #include "std/error.h"
 #include "std/io.h"
 #include "std/memory.h"
 #include "std/strings.h"
 #include <stdint.h>
-
-const uint32_t g_gsm_guard = 0x0a0d0d0a;
-const uint32_t g_gsm_magic = 0x4F434247;
-const uint8_t g_gsm_major = 1;
-const uint8_t g_gsm_minor = 0;
-const uint8_t g_gsm_patch = 0;
-const uint8_t g_gsm_unused = 0;
 
 int32_t gsm_first_pass(std_file *restrict ga_file, std_file *restrict store);
 int32_t gsm_second_pass(std_file *restrict ga_file,
@@ -54,6 +48,7 @@ int main(int argc, const char **argv) {
 
     if (arg.type == ARG_ARGUMENT) { // First arg is the name of the file.
       file_name = arg.argument.val;
+      break;
     }
   } while (arg.type != ARG_END);
 
@@ -105,8 +100,24 @@ int32_t gsm_first_pass(std_file *restrict ga_file, std_file *restrict store) {
   return 0;
 }
 
+// Gets the opcode integer from its string representation. This opcode needs
+// to be shifted before being placed into an instruction.
+uint32_t get_opcode(std_string opcode);
+
+// Gets the register number from its string representation. This register
+// needs to be shifted before being placed into an instruction.
+uint32_t get_register(std_string reg);
+
+// Gets the func from a code 1 opcode string. This func needs to be shifted
+// before being placed into an instruction.
+uint32_t get_code1_func(std_string opcode);
+
+// Gets the func from a code 3 opcode string. This func needs to be shifted
+// before being placed into an instruction.
+uint32_t get_code3_func(std_string opcode);
+
 // Performs a GSM second pass on [ga_file] and stores the resulting binary in
-// [gbc]. Returns 0 on a success, and a non-zero error code on failure.
+// [gbc_file]. Returns 0 on a success, and a non-zero error code on failure.
 int32_t gsm_second_pass(std_file *restrict ga_file,
                         std_file *restrict gbc_file) {
 
@@ -136,8 +147,11 @@ int32_t gsm_second_pass(std_file *restrict ga_file,
   if (file_err(gbc_file))
     return file_err(gbc_file);
 
-  // Begin reading lines from the GA file
-  std_arena *line_memory = arena_create(0, 0);
+  std_arena *line_memory = arena_create(sizeof(int32_t), 0);
+
+  // TODO: Read the data section of the GBC file
+
+  // Begin reading code lines from the GA file
   while (file_err(ga_file) == 0) {
     std_string line = file_read_line(line_memory, ga_file);
 
@@ -146,7 +160,131 @@ int32_t gsm_second_pass(std_file *restrict ga_file,
     str_tokenize(token, line, space);
 
     // TODO: Read tokens from a GA line and write its bytecode to the gbc file.
+    gbc_command command;
+
+    uint32_t bytecode;
+    std_string opcode_string = str_token_get(token);
+
+    uint32_t opcode = get_opcode(opcode_string);
+    bytecode |= opcode << OPCODE_SHFT;
+
+    switch (opcode) {
+    case OPCODE_CODE1: // imm | rx1 | func | opcode
+      uint32_t func = get_code1_func(opcode_string);
+      bytecode |= func << CODE1_FNC_SHFT;
+
+      uint32_t rx1 = get_register(opcode_string);
+      bytecode |= rx1 << CODE1_RX1_SHFT;
+
+      uint32_t imm = bytecode & CODE1_IMM;
+
+    case OPCODE_CODE3: // rx3 | rx2 | rx1 | 000 | func | opcode
+
+    default: // imm | rx2 | rx1 | opcode
+    }
+
+    arena_clean(line_memory);
   }
 
+  arena_destroy(line_memory);
   return 0;
+}
+
+typedef struct {
+  const char *name;
+  uint32_t code;
+} code_mapping;
+
+uint32_t get_opcode(std_string opcode) {
+  static const code_mapping opcode_mappings[] = {
+      {"ldu", OPCODE_CODE1},  {"jr", OPCODE_CODE1},   {"jrc", OPCODE_CODE1},
+      {"jrcx", OPCODE_CODE1}, {"push", OPCODE_CODE1}, {"pop", OPCODE_CODE1},
+      {"zero", OPCODE_CODE1}, {"sys", OPCODE_CODE1},
+
+      {"add", OPCODE_CODE3},  {"sub", OPCODE_CODE3},  {"mul", OPCODE_CODE3},
+      {"mulu", OPCODE_CODE3}, {"div", OPCODE_CODE3},  {"blx", OPCODE_CODE3},
+      {"brx", OPCODE_CODE3},  {"brxu", OPCODE_CODE3}, {"and", OPCODE_CODE3},
+      {"or", OPCODE_CODE3},   {"xor", OPCODE_CODE3},  {"eq", OPCODE_CODE3},
+      {"gt", OPCODE_CODE3},   {"lt", OPCODE_CODE3},   {"eqx", OPCODE_CODE3},
+      {"gtx", OPCODE_CODE3},  {"ltx", OPCODE_CODE3},  {"addf", OPCODE_CODE3},
+      {"subf", OPCODE_CODE3}, {"mulf", OPCODE_CODE3}, {"mulfu", OPCODE_CODE3},
+      {"divf", OPCODE_CODE3},
+
+      {"addi", OPCODE_ADDI},  {"subi", OPCODE_SUBI},  {"bli", OPCODE_BLI},
+      {"bri", OPCODE_BRI},    {"briu", OPCODE_BRIU},  {"andi", OPCODE_ANDI},
+      {"ori", OPCODE_ORI},    {"xori", OPCODE_XORI},  {"lw", OPCODE_LW},
+      {"lh", OPCODE_LH},      {"lb", OPCODE_LB},      {"sw", OPCODE_SW},
+      {"sh", OPCODE_SH},      {"sb", OPCODE_SB},
+  };
+
+  uint32_t opcode_table_size =
+      sizeof(opcode_mappings) / sizeof(opcode_mappings[0]);
+  for (uint32_t i = 0; i < opcode_table_size; i++) {
+    if (str_compare(opcode, str(opcode_mappings[i].name)) == 0) {
+      return opcode_mappings[i].code;
+    }
+  }
+
+  std_panic("Invalid opcode read: %s", str_get(opcode));
+}
+
+uint32_t get_register(std_string reg) {
+  static const code_mapping reg_mappings[] = {
+      {"ax", R_AX},   {"bx", R_BX},   {"cx", R_CX},   {"dx", R_DX},
+      {"ex", R_EX},   {"fx", R_FX},   {"gx", R_GX},   {"hx", R_HX},
+
+      {"sax", R_SAX}, {"sbx", R_SBX}, {"scx", R_SCX}, {"sdx", R_SDX},
+
+      {"ret", R_RET}, {"up", R_UP},   {"0", R_ZERO},  {"if", R_IF},
+      {"tg", R_TG},   {"sp", R_SP},   {"srg", R_SRG}, {"asm", R_ASM},
+      {"pc", R_PC},
+  };
+
+  uint32_t reg_table_size = sizeof(reg_mappings) / sizeof(reg_mappings[0]);
+  for (uint32_t i = 0; i < reg_table_size; i++) {
+    if (str_compare(reg, str(reg_mappings[i].name)) == 0) {
+      return reg_mappings[i].code;
+    }
+  }
+
+  std_panic("Invalid register read: %s", str_get(reg));
+}
+
+uint32_t get_code1_func(std_string opcode) {
+  static const code_mapping func_mappings[] = {
+      {"ldu", FUNC_LDU},   {"jr", FUNC_JR},     {"jrc", FUNC_JRC},
+      {"jrcx", FUNC_JRCX}, {"push", FUNC_PUSH}, {"pop", FUNC_POP},
+      {"zero", FUNC_ZERO}, {"sys", FUNC_SYS},
+  };
+
+  uint32_t func_table_size = sizeof(func_mappings) / sizeof(func_mappings[0]);
+  for (uint32_t i = 0; i < func_table_size; i++) {
+    if (str_compare(opcode, str(func_mappings[i].name)) == 0) {
+      return func_mappings[i].code;
+    }
+  }
+
+  std_panic("Invalid opcode read: %s", str_get(opcode));
+}
+
+uint32_t get_code3_func(std_string opcode) {
+  static const code_mapping func_mappings[] = {
+      {"add", FUNC_ADD},   {"sub", FUNC_SUB},   {"mul", FUNC_MUL},
+      {"mulu", FUNC_MULU}, {"div", FUNC_DIV},   {"blx", FUNC_BLX},
+      {"brx", FUNC_BRX},   {"brxu", FUNC_BRXU}, {"and", FUNC_AND},
+      {"or", FUNC_OR},     {"xor", FUNC_XOR},   {"eq", FUNC_EQ},
+      {"gt", FUNC_GT},     {"lt", FUNC_LT},     {"eqx", FUNC_EQX},
+      {"gtx", FUNC_GTX},   {"ltx", FUNC_LTX},   {"addf", FUNC_ADDF},
+      {"subf", FUNC_SUBF}, {"mulf", FUNC_MULF}, {"mulfu", FUNC_MULFU},
+      {"divf", FUNC_DIVF},
+  };
+
+  uint32_t func_table_size = sizeof(func_mappings) / sizeof(func_mappings[0]);
+  for (uint32_t i = 0; i < func_table_size; i++) {
+    if (str_compare(opcode, str(func_mappings[i].name)) == 0) {
+      return func_mappings[i].code;
+    }
+  }
+
+  std_panic("Invalid opcode read: %s", str_get(opcode));
 }

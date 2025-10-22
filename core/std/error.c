abbc6727808ab92ee74531b222e37b35fcc850c4
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2025-10-21 23:54:58 -0400

    Fix assert error messages

--- core/std/error.c
+++ core/std/error.c
@@ -3,9 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void errno_msg(const char *msg) {
-  perror(msg);
-}
+void errno_msg(const char *msg) { perror(msg); }
 
 __attribute__((format(printf, 1, 2))) int eprintf(const char *restrict format,
                                                   ...) {
@@ -20,7 +18,7 @@ __attribute__((format(printf, 1, 2))) int eprintf(const char *restrict format,
 _Noreturn void _std_builtin_assert(const char *filename, const char *func,
                                    int line, int expr, const char *err,
                                    const char *format, ...) {
-  eprintf("Assertion failed: function %s, file %s, line %d\n%s: .\n", func,
+  eprintf("Assertion failed: function %s, file %s, line %d\n%s: ", func,
           filename, line, err);
 
   va_list args;
@@ -33,7 +31,7 @@ _Noreturn void _std_builtin_assert(const char *filename, const char *func,
 
 _Noreturn void _std_builtin_panic(const char *filename, const char *func,
                                   int line, const char *format, ...) {
-  eprintf("Panic:");
+  eprintf("Panic: ");
 
   va_list args;
   va_start(args, format);
